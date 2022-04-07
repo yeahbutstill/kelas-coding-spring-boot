346f4c21456f72cdf5c0b15ec13d0109743c7ef7
yeahbutstill <mayadani28@gmail.com>
2022-04-07 18:52:43 +0700

    Constructor and Setter Injection

--- src/main/java/com/yeahbutstill/demorestapi/services/impl/BinarySearchImpl.java
+++ src/main/java/com/yeahbutstill/demorestapi/services/impl/BinarySearchImpl.java
@@ -10,12 +10,6 @@ public class BinarySearchImpl {
     @Autowired
     private SortAlgorithm sortAlgorithm;
 
-    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-
-        this.sortAlgorithm = sortAlgorithm;
-
-    }
-
     public int binarySearch(int[] numbers, int numberToSearchFor) {
 
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
