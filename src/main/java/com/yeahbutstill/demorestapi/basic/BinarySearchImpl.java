9787f6c6dd9d7627cc0fe78292cdc94485842ddd
yeahbutstill <mayadani28@gmail.com>
2022-04-08 02:18:07 +0700

    Lifecycle of a Bean - @PostConstruct and @PreDestroy

--- src/main/java/com/yeahbutstill/demorestapi/basic/BinarySearchImpl.java
+++ src/main/java/com/yeahbutstill/demorestapi/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.yeahbutstill.demorestapi.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) // by default is singleton
 public class BinarySearchImpl {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
 //    @Qualifier("quick")
@@ -22,6 +29,20 @@ public class BinarySearchImpl {
 
         // Return the result
         return 10;
+
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+
+        LOGGER.info("postConstruct");
+
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+
+        LOGGER.info("preDestroy");
 
     }
 
